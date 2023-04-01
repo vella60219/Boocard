cdfa355d1e50a02295f1932fe0fb88f809b0a47d
vella60219 <83958906+vella60219@users.noreply.github.com>
2023-04-01 19:17:54 +0800

    add some link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 // 首頁
 // boocard.vercel.app/
 
+import { Link } from "react-router-dom";
+
 function Home() {
     return (
         <div>
             <p>This is Home page</p>
+            <Link to={`/event/`}><p>活動總覽</p></Link>
+            <Link to={`static/help`}><p>說明</p></Link>
+            <Link to={`/static/how`}><p>使用教學</p></Link>
         </div>
     );
 }
