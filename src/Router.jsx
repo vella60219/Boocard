88b9e62f232aca0dbe31542c0bf6022561c638af
vella60219 <83958906+vella60219@users.noreply.github.com>
2023-04-01 18:29:05 +0800

    router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,7 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
+
+import Help from "./pages/Help";
+import How from "./pages/How";
+
+import AllEvent from "./pages/AllEvent";
 import Event from "./pages/Event";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function Router() {
 
@@ -10,14 +18,17 @@ function Router() {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/static" >     {/* 靜態網頁們（非實際頁面） */}
-                    <Route path="/help" element={<Home />} />   {/* 說明（幫助）頁面 */}
-                    <Route path="/how" element={<Home />} />    {/* 使用教學（遊戲方式）頁面 */}
+                    <Route path="/help" element={<Help />} />   {/* 說明（幫助）頁面 */}
+                    <Route path="/how" element={<How />} />    {/* 使用教學（遊戲方式）頁面 */}
                 </Route>
-                <Route path="/event" element={<Home />} >
-
+                <Route path="/event" element={<AllEvent />} >    {/* 活動總覽頁面 */}
+                    <Route path="/tag/:tagName" element={<AllEvent />} />  {/* 活動搜尋（tag）頁面 */}
+                    <Route path="/search/:searchKeyword" element={<AllEvent />} />  {/* 活動搜尋（關鍵字）頁面 */}
+                    <Route path="/:eventId" element={<Event />} />  {/* 活動主頁 */}
                 </Route>
                 <Route path="/user" >
-
+                    <Route path="/login" element={<Login />} />     {/* 登入頁面 */}
+                    <Route path="/signup" element={<Signup />} />   {/* 註冊頁面 */}
                 </Route>
             </Routes>
         </BrowserRouter>
---
+++ src/pages/How.jsx
@@ -0,0 +1,12 @@
+// 使用教學（遊戲方式）頁面
+// boocard.vercel.app/static/how
+
+function How() {
+    return (
+        <div>
+            <p>This is How page</p>
+        </div>
+    );
+}
+
+export default How;
