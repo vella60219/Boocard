b5922d23d5ccf5c335c89d115874bbd4d2c12e08
vella60219 <83958906+vella60219@users.noreply.github.com>
2023-04-06 23:04:18 +0800

    EventItem css maybe ok

--- src/components/EventItem/index.jsx
+++ src/components/EventItem/index.jsx
@@ -7,46 +7,50 @@ const event = events[0];
 const { Meta } = Card;
 function EventItem() {
     return (
-        <Link to >
-            <Card
-                hoverable
-                className={styles.box}
-                size='small'
-                bordered="true"
-                cover={<img
-                    alt={event.name}
-                    src={event.image}
+        <div className="borderBox borderBox3">
+            <Link to >
+                {/* <div className="box">
+                <img
                     className={styles.image}
-                />}
-            >
-                <p className={`h3 ${styles.Stxv}`}>
-                    {event.time}<br/>
-                    {event.location}
-                </p>
-                <Meta title={event.name} className={`h2 ${styles.Xtxv}`} />
-                <p>tags</p>
-            </Card>
-        </Link>
+                    src={event.image}
+                    alt={event.name}
+                />
+                <div className={styles.info}>
+                    <p className={styles.Stxv}>
+                        {event.time}
+                    </p>
+                    <p className={styles.Stxv}>
+                        {event.location}
+                        6666
+                    </p>
+                    <p className={styles.Xtxv}>
+                        {event.name}
+                    </p>
+                </div>
+                </div> */}
+                <Card
+                    
+                    className={styles.box}
+                    size='small'
+                    
+                    cover={<img
+                        alt={event.name}
+                        src={event.image}
+                        className={styles.image}
+                    />}
+                >
+                    <p className={`h3 ${styles.Stxv}`}>
+                        {event.time}<br />
+                        {event.location}
+                    </p>
+                    <Meta title={event.name} className={`h2 ${styles.Xtxv}`} />
+                    <p>tags</p>
+                </Card>
+            </Link>
+        </div>
     );
 }
 {/* <Link to >
-                <img
-                    className={styles.image}
-                    src={eventinfo.image}
-                    alt={eventinfo.name}
-                />
-            </Link>
-            <div className={styles.info}>
-                <p className={styles.Stxv}>
-                    {eventinfo.time}
-                </p>
-                <p className={styles.Stxv}>
-                    {eventinfo.location}
-                    6666
-                </p>
-                <p className={styles.Xtxv}>
-                    {eventinfo.name}
-                </p>
-            </div> */}
+                 */}
 
 export default EventItem;
