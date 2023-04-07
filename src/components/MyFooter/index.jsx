640f81c83302723c8745b042f48b168320257883
lq-jw <a21954262@gmail.com>
2023-04-07 16:17:24 +0800

    font_size_setting

--- src/components/MyFooter/index.jsx
+++ src/components/MyFooter/index.jsx
@@ -11,7 +11,7 @@ function MyFooter() {
                     lg = {{ span: 10}}
                 >
                     <div>
-                        <p className={styles.Stxv}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. </p>
+                        <h3 className={styles.Stxv}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla, mattis ligula consectetur, ultrices mauris. </h3>
                     </div>
                     <div className={`${styles.btnSection} ${styles.box}`}>
                         <a href='https://zh-tw.facebook.com/'>
@@ -30,32 +30,32 @@ function MyFooter() {
                     sm = {{ span: 24}}
                     lg = {{ span: 3}}
                 >
-                    <p className={styles.Htxv}>關於我們</p>
-                    <p className={styles.Stxv}>服務條款</p>
-                    <p className={styles.Stxv}>隱私權政策</p>
+                    <h2 className={styles.Htxv}>關於我們</h2>
+                    <h3 className={styles.Stxv}>服務條款</h3>
+                    <h3 className={styles.Stxv}>隱私權政策</h3>
                 </Col>
                 <Col 
                     xs = {{ span: 24}}
                     sm = {{ span: 24}}
-                    lg = {{ span: 4}}
+                    lg = {{ span: 5}}
                 >   
-                    <p className={styles.Htxv}>聯絡資訊</p>
-                    <p className={styles.Stxv}>TEL：03-5223118</p>
-                    <p className={styles.Stxv}>Email：jioejf@jis.com.tw</p>
-                    <p className={styles.Stxv}>300 台北郵政0號信箱</p>
+                    <h2 className={styles.Htxv}>聯絡資訊</h2>
+                    <h3 className={styles.Stxv}>TEL：03-5223118</h3>
+                    <h3 className={styles.Stxv}>Email：jioejf@jis.com.tw</h3>
+                    <h3 className={styles.Stxv}>300 台北郵政0號信箱</h3>
                 </Col>
                 <Col
                     xs = {{ span: 24}}
                     sm = {{ span: 24}}
                     lg = {{ span: 4}}
                 >
-                    <p className={styles.Htxv}>其他服務</p>
-                    <p className={styles.Stxv}>C.W.T. 活動網站</p>
-                    <p className={styles.Stxv}>C.W.T. 社團刊物廣告</p>
+                    <h2 className={styles.Htxv}>其他服務</h2>
+                    <h3 className={styles.Stxv}>C.W.T. 活動網站</h3>
+                    <h3 className={styles.Stxv}>C.W.T. 社團刊物廣告</h3>
                 </Col>
             </Row>
             <div className={styles.Copy}>
-                <p className={styles.Copytxv}>© 2023 Boocard copyright</p>
+                <h3 className={styles.Copytxv}>© 2023 Boocard copyright</h3>
             </div>
         </div>
     );
