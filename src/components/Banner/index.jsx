eabeaeb8f15351230ef74b45f45b5cb33617a619
vella60219 <83958906+vella60219@users.noreply.github.com>
2023-04-10 20:38:09 +0800

    banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,17 +1,17 @@
-import style from'./banner.module.css'
+import style from './banner.module.css'
 import { Button } from 'antd';
 import { useParams, Link } from "react-router-dom";
 
-function HomeBanner(){
-    return(
+function HomeBanner() {
+    return (
         <div className={style.H}>
             <div>
                 <div >
                     <img
                         className={style.HImg}
-                        src='./images/img_banner.png' 
+                        src='./images/img_banner-low.png'
                         alt='banner'
-                    />            
+                    />
                 </div>
 
                 <div className={style.Htxv}>
@@ -20,22 +20,22 @@ function HomeBanner(){
                         src='./images/logo_H.png'
                         alt='logo'
                     />
-                    <h2 className={style.Hxv_intro}>帶您在攤位的海洋中，掌握每個攤位的資訊，<br/> 快速找到您喜愛的創作者的攤位與商品！</h2>
+                    <h2 className={`h2 ${style.Hxv_intro}`}>帶您在攤位的海洋中，掌握每個攤位的資訊，<br /> 快速找到您喜愛的創作者的攤位與商品！</h2>
                     <div className={style.btn}>
                         <Link
-                           
+
                             className={` h2 ${style.freebtn}`}
                         >
                             免費註冊
                         </Link>
                         <Link
 
-                           className={` h2 ${style.howbtn}`}
-                       >
-                           如何使用
-                       </Link>
+                            className={` h2 ${style.howbtn}`}
+                        >
+                            如何使用
+                        </Link>
                     </div>
-                </div>                
+                </div>
             </div>
 
         </div>
@@ -43,4 +43,4 @@ function HomeBanner(){
 }
 
 
-export {HomeBanner};
+export { HomeBanner };
