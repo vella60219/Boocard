3d425dc59c4a184554aa360b65f15f0a77ea56b7
lq-jw <a21954262@gmail.com>
2023-04-10 23:03:22 +0800

    creat float button tooltip

--- src/components/FloatButton/index.jsx
+++ src/components/FloatButton/index.jsx
@@ -1,6 +1,8 @@
 import { FloatButton } from 'antd';
 import { AiOutlineVerticalAlignTop } from "react-icons/ai";
 import { MdBookmarks } from "react-icons/md";
+import { useSelector, useDispatch } from 'react-redux';
+import { selectLoginMode, setLoginMode } from '../../redux/loginSlice';
 
 function BackTop() {
     return (
@@ -21,12 +23,22 @@ function BackTop() {
 }
 
 function FloatGrop() {
+    const LoginMode = useSelector(selectLoginMode);
     return (
+        <>
+            {LoginMode ?
+                <FloatButton.Group shape="circle" style={{ right: 24 }}>
+                    <FloatButton icon={<MdBookmarks style={{ color: "#fff", height: "25px", width: "25px" }}/> } type="primary" />
+                    <FloatButton.BackTop icon={<AiOutlineVerticalAlignTop style={{ color: "#FFA1B8", height: "30px", width: "30px" }} />} visibilityHeight={0} />
+                </FloatButton.Group>:
+                <FloatButton.Group shape="circle" style={{ right: 24 }}>
+                    <FloatButton tooltip={ <div>請先登入</div>} icon={<MdBookmarks style={{ color: "#fff", height: "25px", width: "25px" }}/> } type="primary" />
+                    <FloatButton.BackTop icon={<AiOutlineVerticalAlignTop style={{ color: "#FFA1B8", height: "30px", width: "30px" }} />} visibilityHeight={0} />
+                </FloatButton.Group>    
+            }
+     
+        </>
 
-        <FloatButton.Group shape="circle" style={{ right: 24 }}>
-            <FloatButton icon={<MdBookmarks style={{ color: "#fff", height: "25px", width: "25px" }} />} type="primary" />
-            <FloatButton.BackTop icon={<AiOutlineVerticalAlignTop style={{ color: "#FFA1B8", height: "30px", width: "30px" }} />} visibilityHeight={0} />
-        </FloatButton.Group>
 
     );
 }
