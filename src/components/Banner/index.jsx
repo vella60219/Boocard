978bd4b38b1bdf60ed23895ca02db158e26f3694
vella60219 <83958906+vella60219@users.noreply.github.com>
2023-04-10 22:33:45 +0800

    Introduction rwd

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -5,40 +5,36 @@ import { useParams, Link } from "react-router-dom";
 function HomeBanner() {
     return (
         <div className={style.H}>
-            <div>
-                <div >
-                    <img
-                        className={style.HImg}
-                        src='./images/img_banner-low.png'
-                        alt='banner'
-                    />
-                </div>
+            <img
+                className={style.HImg}
+                src='./images/img_banner-low.png'
+                alt='banner'
+            />
 
-                <div className={style.Htxv}>
-                    <img
-                        className={style.HLogo}
-                        src='./images/logo_H.png'
-                        alt='logo'
-                    />
-                    <h2 className={`h2 ${style.Hxv_intro}`}>帶您在攤位的海洋中，掌握每個攤位的資訊，<br /> 快速找到您喜愛的創作者的攤位與商品！</h2>
-                    <div className={style.btn}>
-                        <Link
+            <div className={style.Htxv}>
+                <img
+                    className={style.HLogo}
+                    src='./images/logo_H.png'
+                    alt='logo'
+                />
+                <h2 className={`h2 ${style.Hxv_intro}`}>帶您在攤位的海洋中，掌握每個攤位的資訊，<br /> 快速找到您喜愛的創作者的攤位與商品！</h2>
+                <div className={style.btn}>
+                    <Link
 
-                            className={` h2 ${style.freebtn}`}
-                        >
-                            免費註冊
-                        </Link>
-                        <Link
+                        className={` h2 ${style.freebtn}`}
+                    >
+                        免費註冊
+                    </Link>
+                    <Link
 
-                            className={` h2 ${style.howbtn}`}
-                        >
-                            如何使用
-                        </Link>
-                    </div>
+                        className={` h2 ${style.howbtn}`}
+                    >
+                        如何使用
+                    </Link>
                 </div>
             </div>
-
         </div>
+
     )
 }
 
