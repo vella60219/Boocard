432dfb6e4518f61df471f6a3a7a37365ed2ec91d
vella60219 <83958906+vella60219@users.noreply.github.com>
2023-05-29 02:34:34 +0800

    card title

--- src/components/BoothInfoPage/index.jsx
+++ src/components/BoothInfoPage/index.jsx
@@ -1,19 +1,40 @@
 // 攤位資訊頁面，從這裡可以點進去攤主的自我介紹頁面和品項頁面
 import styles from './boothInfoPage.module.css';
 import { Empty } from 'antd';
+import { Link, useSearchParams } from 'react-router-dom';
 import BoothGoodsItem from '../BoothGoodsItem';
 import BoothOwnerItem from '../BoothOwnerItem';
 
+import events from '../../json/events.json'
+
 function BoothInfoPage({ booth }) {
+    const event = events.find(
+        (x) => x.id === booth.eventID
+    );
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    const act = searchParams.get("act");
+    const _act = act === null ? booth.booths[0] : booth.booths.find(
+        (x) => x.actID.toUpperCase() === act.toUpperCase()
+    );
 
     return (
-        <div>
-            <p>攤位ID：{booth.id}</p>
+        <div className={styles.box}>
+            <h1 className={`h1 ${styles.boothName}`}>{booth.booth_name}</h1>
+            <div>
+                <span className={`h3 ${styles.eventName}`}>{event.name}</span>
+                <span className="h2">
+                    <Link>#{_act.zone}{_act.number}</Link>
+                    {/* ^ 預計會像噗浪一樣弄出一個新頁面 */}
+                </span>
+            </div>
+            {/* ^ 標題 */}
+            <BoothOwnerItem />
             {/* <Empty description={false} /> */}
             <div style={{ height: '400px' }} />
             {/* ^ 測試資料用 */}
             <BoothGoodsItem />
-            <BoothOwnerItem />
+
         </div>
     );
 }
